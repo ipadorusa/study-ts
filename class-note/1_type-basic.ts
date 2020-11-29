b25c3461e877889cef941190ea58f82f6c5bddfe
woosukkim <ipadorusa@gmail.com>
2020-11-29 22:19:01 +0900

    study02

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,25 @@
+//var str = 'hello';
+
+// string
+var str: string = 'hello';
+// number
+let num: number = 10;
+
+// Array
+let arr: Array<number> = [1,2,3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk']
+let items: number[] = [1,2,3];
+
+// tuple
+let address: [string, string] = ['gangnam', 'pangyo'];
+
+// object
+let obj: object = {};
+let person: {name: string, age: number} = {
+	name: 'capt',
+	age: 100,
+};
+
+// boolean
+
+let show: boolean = true;
